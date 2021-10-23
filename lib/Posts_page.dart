1d147a038d305f7474f5880ba187dad1db8c6140
solutionflutter <solutionflutter@gmail.com>
2021-10-23 15:02:48 +0600

    23/10/2021 first commit in satmonwedsecondbatch

--- lib/Posts_page.dart
+++ lib/Posts_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/cupertino.dart';
+import 'package:satmonwedsecondbatch/profile_page.dart';
 
 class PostsPage extends StatefulWidget {
   const PostsPage({Key key}) : super(key: key);
@@ -11,6 +12,51 @@ class PostsPage extends StatefulWidget {
 class _PostsPageState extends State<PostsPage> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      appBar: AppBar(
+        leading: IconButton(
+          icon: Icon(
+            Icons.arrow_back_ios_outlined,
+            color: Color.fromRGBO(25, 25, 45, .6),
+          ),
+          iconSize: 25,
+          onPressed: (){
+            Navigator.push(
+              context,
+              MaterialPageRoute(
+                builder: (context)=>ProfilePage(),
+              ),
+            );
+          },
+        ),
+        title: Text(
+            "Posts Page",
+          style: TextStyle(
+            fontWeight: FontWeight.w700,
+            fontSize: 25,
+            color: Color.fromRGBO(25, 25, 45, .6)
+          ),
+        ),
+        centerTitle: true,
+        actions: [
+          IconButton(
+            icon: Icon(
+              Icons.refresh_outlined,
+              color: Color.fromRGBO(25, 25, 45, .6),
+            ),
+            iconSize: 25,
+            onPressed: (){
+              Navigator.push(
+                context,
+                MaterialPageRoute(
+                  builder: (context)=>PostsPage(),
+                ),
+              );
+            },
+          )
+        ],
+        backgroundColor: Color.fromRGBO(240, 240, 240, .6),
+      ),
+    );
   }
 }
